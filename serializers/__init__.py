c7be36b4f03200501416b4f40ab0cbf3d37896fa
Cyrille07 <bio.kouma@edu.esiee.fr>
2025-12-02 22:11:18 +0100

    Delete serializers directory

--- serializers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .employee import EmployeeOutput, EmployeeCreate, EmployeeOutputMini
-from .task import TaskOutput, TaskCreate, TaskOutputMini
-
-# résolution des types circulaires
-TaskOutput.model_rebuild()
-EmployeeOutput.model_rebuild()
